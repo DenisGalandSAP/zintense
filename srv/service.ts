d2cddf58584b726bb4b9d778fe37d57daee924a6
Denis Galand <denis.galand@contractor.resa.be>
2026-01-09 09:45:48 +0000

    unifed read and create request

--- srv/service.ts
+++ srv/service.ts
@@ -4,11 +4,7 @@ export default class IntenseService extends cds.ApplicationService {
     async init() {
         const ZGP_PM_INTENSE_SRV = await cds.connect.to('ZGP_PM_INTENSE_SRV');
 
-        this.on('READ', 'LinkEquipementEANSet', async (req) => {
-            return ZGP_PM_INTENSE_SRV.run(req.query);
-        });
-
-        this.on('CREATE', 'LinkEquipementEANSet', async (req) => {
+        this.on('*', 'LinkEquipementEANSet', async (req) => {
             return ZGP_PM_INTENSE_SRV.run(req.query);
         });
 
